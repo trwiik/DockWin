14d4bd87db5025413a19db4373a54d9ac05aecc7
Trond Wiik <trond@laud-media.com>
2018-06-21 02:07:37 +0200

    Restore windowState

--- DockWin/FormMain.cs
+++ DockWin/FormMain.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
 
 namespace DockWin
@@ -16,12 +18,85 @@ namespace DockWin
         [DllImport("user32.dll")]
         private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
         private const int HIDE = 0;
-        private const int MINIMIZED = 1;
-        private const int MAXIMIZED = 2;
-        private const int RESTORE = 9;
+        //private const int MINIMIZED = 1;
+        private const int MINIMIZED = 2; // Show minimized
+        //private const int MAXIMIZED = 2;
+        private const int MAXIMIZED = 3; // Show maximized
+        //private const int RESTORE = 9;
+        private const int RESTORE = 1; // Show normal
+
+        //private static List<IntPtr> hWindList;
+        private static List<WindowParams> mWindowsParams;
 
 
-        private List<WindowParams> mWindowsParams;
+        /*TEST*/
+        protected delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        protected static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        protected static extern int GetWindowTextLength(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        protected static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+        [DllImport("user32.dll")]
+        protected static extern bool IsWindowVisible(IntPtr hWnd);
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint processId);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowPlacement(IntPtr hWnd, ref WINDOWPLACEMENT lpwndpl);
+
+        private struct WINDOWPLACEMENT
+        {
+            public int length;
+            public int flags;
+            public int showCmd;
+            public Point ptMinPosition;
+            public Point ptMaxPosition;
+            public Rectangle rcNormalPosition;
+        }
+
+        protected static bool EnumTheWindows(IntPtr hWnd, IntPtr lParam)
+        {
+            int size = GetWindowTextLength(hWnd);
+            if (size++ > 0 && IsWindowVisible(hWnd))
+            {
+                StringBuilder sb = new StringBuilder(size);
+                GetWindowThreadProcessId(hWnd, out uint processId);
+                GetWindowText(hWnd, sb, size);
+                String title = sb.ToString();
+                //Console.WriteLine("PID=" + processId + ": " + sb.ToString());
+                //hWindList.Add(hWnd);
+                Process p = Process.GetProcessById((int)processId);
+                WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+                GetWindowPlacement(hWnd, ref placement);
+                int windowState = RESTORE; // Normal
+                switch (placement.showCmd)
+                {
+                    case 0:
+                        windowState = HIDE;
+                        break;
+                    case 2:
+                        windowState = MINIMIZED;
+                        break;
+                    case 3:
+                        windowState = MAXIMIZED;
+                        break;
+                }
+                GetWindowRect(hWnd, out Rectangle rect);
+                mWindowsParams.Add(new WindowParams()
+                {
+                    hWndPtr = hWnd,
+                    processName = "",
+                    title = title,
+                    processId = processId,
+                    rect = rect,
+                    windowState = windowState
+                });
+            }
+            return true;
+        }
+        /*TEST END*/
+
 
         public FormMain()
         {
@@ -92,48 +167,60 @@ namespace DockWin
 
         private void SaveWindows()
         {
-            TxtLog.Clear();
             mWindowsParams = new List<WindowParams>();
+            EnumWindows(new EnumWindowsProc(EnumTheWindows), IntPtr.Zero);
+
+            // Has now hWindList
+
+            TxtLog.Clear();
+//            mWindowsParams = new List<WindowParams>();
 
             String txt = "SAVE:" + Environment.NewLine;
-            Process[] processlist = Process.GetProcesses();
+            txt += "Num windows: " + mWindowsParams.Count + Environment.NewLine;
+            //var processlist = Process.GetProcesses()
+            //    .Where(p => p.MainWindowHandle != IntPtr.Zero);
 
-            foreach (Process process in processlist)
-            {
-                if (!String.IsNullOrEmpty(process.MainWindowTitle))
-                {
-                    IntPtr hwnd = process.MainWindowHandle;
-                    GetWindowRect(hwnd, out Rectangle rect);
-                    if (rect.Top == 0 && rect.Left == 0)
-                        continue;
+            //foreach (Process process in processlist)
+            //{
+            foreach (WindowParams winParam in mWindowsParams)
+            { 
+                //if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                //{
+                    //IntPtr hwnd = process.MainWindowHandle;
+                    //GetWindowRect(winParam.hWndPtr, out Rectangle rect);
+                    //if (winParam.rect.Top == 0 && winParam.rect.Left == 0)
+                    //    continue;
 
-                    if (process.ProcessName == "ApplicationFrameHost")
-                        continue;
+                    //if (process.ProcessName == "ApplicationFrameHost")
+                    //    continue;
 
-                    if (rect.Top > -1000) // Else assumed minimized
-                    {
-                        txt += String.Format("Saving process '{0}' (id {1})(title '{2}': ",
-                            process.ProcessName,
-                            process.Id,
-                            process.MainWindowTitle);
+                    //if (winParam.rect.Top > -1000) // Else assumed minimized
+                    //{
+                    //txt += String.Format("Saving process '{0}' (id {1})(title '{2}': ",
+                    //    process.ProcessName,
+                    //    process.Id,
+                    //    process.MainWindowTitle);
 
-                        txt += "Top=" + rect.Top;
-                        txt += ",Left=" + rect.Left;
-                        txt += ",Height=" + rect.Height;
-                        txt += ",Width=" + rect.Width;
+                        txt += "Title '" + winParam.title + "': ";
+                        txt += "Top=" + winParam.rect.Top;
+                        txt += ",Left=" + winParam.rect.Left;
+                        txt += ",Height=" + winParam.rect.Height;
+                        txt += ",Width=" + winParam.rect.Width;
+                        txt += ",winState=" + winParam.windowState;
                         txt += Environment.NewLine;
 
-                        mWindowsParams.Add(new WindowParams()
-                        {
-                            processName = process.ProcessName,
-                            processId = process.Id,
-                            top = rect.Top,
-                            left = rect.Left,
-                            width = rect.Width,
-                            height = rect.Height
-                        });
-                    }
-                }
+                        //mWindowsParams.Add(new WindowParams()
+                        //{
+                        //    hWndPtr = hwnd,
+                        //    processName = "",
+                        //    processId = 0,
+                        //    top = rect.Top,
+                        //    left = rect.Left,
+                        //    width = rect.Width,
+                        //    height = rect.Height
+                        //});
+                    //}
+                //}
             }
 
             TxtLog.Text = txt;
@@ -149,30 +236,51 @@ namespace DockWin
                 return;
             }
 
-            Process[] processlist = Process.GetProcesses();
-            foreach (Process process in processlist)
+            //Process[] processlist = Process.GetProcesses();
+            //foreach (Process process in processlist)
+            foreach (WindowParams winParam in mWindowsParams)
             {
-                if (String.IsNullOrEmpty(process.MainWindowTitle))
-                    continue;
+                //if (String.IsNullOrEmpty(process.MainWindowTitle))
+                //    continue;
 
-                WindowParams winParam = mWindowsParams.Find(param => param.processName == process.ProcessName);
+                //WindowParams winParam = mWindowsParams.Find(param => param.processName == process.ProcessName);
                 //WindowParams winParam = mWindowsParams.Find(param => param.processId == process.Id);
-                if (winParam == null)
-                {
+                //WindowParams winParam = mWindowsParams.Find(param => param.hWndPtr == hwnd);
+                //if (winParam == null)
+                //{
                     //txt += String.Format("Process'{0}' not found" + Environment.NewLine, process.ProcessName);
+                //    continue;
+                //}
+
+                txt += String.Format("Title '{0}' restoring back to {1}, {2}, {3}, {4}, {5}" + Environment.NewLine,
+                    winParam.title,
+                    winParam.rect.Left,
+                    winParam.rect.Top,
+                    winParam.rect.Width - winParam.rect.Left,
+                    winParam.rect.Height - winParam.rect.Top,
+                    winParam.windowState);
+
+                //IntPtr hwnd = process.MainWindowHandle;
+                if (winParam.windowState == HIDE)
                     continue;
-                }
 
-                txt += String.Format("Process'{0}' restoring back to {1}, {2}, {3}, {4}" + Environment.NewLine,
-                    winParam.processName,
-                    winParam.left,
-                    winParam.top,
-                    winParam.width - winParam.left,
-                    winParam.height - winParam.top);
+                MoveWindow(winParam.hWndPtr, winParam.rect.Left, winParam.rect.Top, winParam.rect.Width - winParam.rect.Left, winParam.rect.Height - winParam.rect.Top, true);
+                ShowWindow(winParam.hWndPtr, winParam.windowState);
 
-                IntPtr hwnd = process.MainWindowHandle;
-                ShowWindow(hwnd, RESTORE);
-                MoveWindow(hwnd, winParam.left, winParam.top, winParam.width - winParam.left, winParam.height - winParam.top, true);
+                //if (winParam.windowState == RESTORE)
+                //{
+                //    MoveWindow(winParam.hWndPtr, winParam.rect.Left, winParam.rect.Top, winParam.rect.Width - winParam.rect.Left, winParam.rect.Height - winParam.rect.Top, true);
+                //    ShowWindow(winParam.hWndPtr, winParam.windowState);
+                //}
+                //else if (winParam.windowState == MAXIMIZED)
+                //{
+                //    ShowWindow(winParam.hWndPtr, winParam.windowState);
+                //    MoveWindow(winParam.hWndPtr, winParam.rect.Left, winParam.rect.Top, 1280, 720, false);
+                //}
+                //else if (winParam.windowState == MINIMIZED)
+                //{
+                //    ShowWindow(winParam.hWndPtr, winParam.windowState);
+                //}
             }
             TxtLog.Text += txt;
         }
--- DockWin/WindowParams.cs
+++ DockWin/WindowParams.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Drawing;
 
 namespace DockWin
 {
     class WindowParams
     {
+        public IntPtr hWndPtr;
         public String processName;
-        public int processId;
-        public int top;
-        public int left;
-        public int width;
-        public int height;
+        public String title;
+        public uint processId;
+        public Rectangle rect;
+        //public int top;
+        //public int left;
+        //public int width;
+        //public int height;
+        public int windowState;
     }
 }
